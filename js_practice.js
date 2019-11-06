670deb4e78a0de4a9a7c7db05adc25ab7d136f16
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-06 21:06:58 +0900

    Playing around with variables

--- js_practice.js
+++ js_practice.js
@@ -4,11 +4,13 @@ y = 2;
 z = x + y;
 
 console.log(z);
+console.log("----------")
 
 var a = "Robert"
 var b = "Hartley"
 
 console.log(a + " " + b);
+console.log("----------")
 
 var c, d, e;
 c = 10;
@@ -16,5 +18,16 @@ d = 20;
 e = c * d;
 
 console.log(e);
-
+console.log("----------")
 // This is how to comment :)
+
+/* 
+WOAH
+WOAH
+WOAH
+*/
+
+var priceOne = 10;
+var priceTwo = 20;
+var totalPrice = priceOne + priceTwo;
+console.log(totalPrice)
