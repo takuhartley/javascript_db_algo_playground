9c60e53677eaf66776da89d8ceb5c0e448d3d92a
Robert Hartley <43013927+RobHart01@users.noreply.github.com>
2019-11-07 21:15:36 +0900

    creating people objects

--- my_world.js
+++ my_world.js
@@ -6,4 +6,20 @@ var world = {
 console.log(world);
 console.log("Hello welcome to " + world.name + ".");
 
+function Person(fn, ln, age, gender, nationality, money) {
+    this.firstName = fn;
+    this.lastName = ln;
+    this.age = age;
+    this.gender = gender;
+    this.nationality = nationality;
+    this.money = money;
+}
 
+var em = new Person("Elon", "Musk", 48, "male", "Canadian", 0);
+console.log(em);
+var mz = new Person("Mark", "Zuckerberg", 35, "male", "American", 0);
+console.log(mz);
+var jb = new Person("Jeff", "Bezos", 55, "male", "American", 0);
+console.log(jb);
+var lp = new Person("Larry", "Page", 46, "male", "American", 0);
+console.log(lp);
